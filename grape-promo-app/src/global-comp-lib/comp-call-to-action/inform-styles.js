33b193bd245a32435e588e714b1da4989615b88c
Sammy Robens-Paradise <srobensparadise@gmail.com>
2019-10-14 21:34:50 -0700

    build and deploy test debugging

--- grape-promo-app/src/global-comp-lib/comp-call-to-action/inform-styles.js
+++ grape-promo-app/src/global-comp-lib/comp-call-to-action/inform-styles.js
@@ -1,7 +1,5 @@
 import styled from "styled-components";
 const InformText = styled.div`
-@import url("https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400&display=swap");
-font-family: "Montserrat", sans-serif;
 font-weight: 100;
 font-size: 18pt;
 color: #ffffff;
