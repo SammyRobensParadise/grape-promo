937b87dcc23d26f481e40656d66d733e952917c7
Sammy Robens-Paradise <srobensparadise@gmail.com>
2019-10-14 22:25:33 -0700

    build and deploy test debugging

--- grape-promo-app/src/global-comp-lib/comp-what-svg/what-svg-styles.js
+++ grape-promo-app/src/global-comp-lib/comp-what-svg/what-svg-styles.js
@@ -1,11 +1,9 @@
 import styled from "styled-components";
 const WhatSVGStyles = styled.div`
-font-weight: 300;
-font-size: 16pt;
-float: left;
-position: relative;
+  font-weight: 300;
+  font-size: 16pt;
+  float: left;
+  position: relative;
 `;
-const CenterEl = styled.div`
-
-`
-export {WhatSVGStyles,CenterEl}
+const CenterEl = styled.div``;
+export { WhatSVGStyles, CenterEl };
