79a02e2053ec9f85e15361af593c1161b4531926
Sammy Robens-Paradise <srobensparadise@gmail.com>
2019-10-14 18:47:31 -0700

    build and deploy test debugging

--- grape-promo-app/src/global-comp-lib/comp-navigation/navbar-styles.js
+++ grape-promo-app/src/global-comp-lib/comp-navigation/navbar-styles.js
@@ -1,18 +1,21 @@
 import styled from "styled-components";
 const NavigationContainer = styled.div`
-  @import url("https://fonts.googleapis.com/css?family=Montserrat:200,300,400&display=swap");
-  font-family: "Montserrat", sans-serif;
-  font-weight: 300;
-  width: 100%;
-  height: 100%;
-  overflowx: hidden;
-  margin-left: 0;
-  margin-right: 0;
-  margin-top: 0;
-  padding-top: 30px;
-  color: #ffffff;
+  &&& {
+    @import url("https://fonts.googleapis.com/css?family=Montserrat:200,300,400&display=swap");
+    font-family: "Montserrat", sans-serif;
+    font-weight: 300;
+    width: 100%;
+    height: 100%;
+    overflowx: hidden;
+    margin-left: 0;
+    margin-right: 0;
+    margin-top: 0;
+    padding-top: 30px;
+    color: #ffffff;
+  }
 `;
 const NavigationBrand = styled.div`
+&&& {
   @import url("https://fonts.googleapis.com/css?family=Montserrat:200,300,400&display=swap");
   font-family: "Montserrat", sans-serif;
   font-weight: 300;
@@ -21,8 +24,10 @@ const NavigationBrand = styled.div`
   margin-right: 35px;
   position: relative;
   float: left;
+}
 `;
 const NavigationChild = styled.span`
+&&& {
   @import url("https://fonts.googleapis.com/css?family=Montserrat:200,300,400&display=swap");
   font-family: "Montserrat", sans-serif;
   font-weight: 300;
@@ -30,5 +35,6 @@ const NavigationChild = styled.span`
   padding-left: 35px;
   margin-right: 35px;
   float: right;
+}
 `;
 export { NavigationContainer, NavigationBrand, NavigationChild };
