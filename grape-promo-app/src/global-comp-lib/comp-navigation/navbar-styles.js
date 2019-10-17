a96b48b0cb9881ad115e5401735466de3c78164a
Sammy Robens-Paradise <srobensparadise@gmail.com>
2019-10-16 21:24:24 -0700

    Updated navigation bar to be mobile responsive... to a point, it could still use some work

--- grape-promo-app/src/global-comp-lib/comp-navigation/navbar-styles.js
+++ grape-promo-app/src/global-comp-lib/comp-navigation/navbar-styles.js
@@ -1,14 +1,17 @@
 import css from "styled-components";
+import * as cssConstants from "../../global-styling/css-constants";
+
 const NavigationContainer = css.div`
     font-weight: 300;
     width: 100%;
     height: 100%;
-    overflowx: hidden;
+    overflow-x: hidden;
     margin-left: 0;
     margin-right: 0;
     margin-top: 0;
     padding-top: 30px;
     color: #ffffff;
+    opacity 0.7;
 `;
 const NavigationBrand = css.div`
   font-weight: 300;
@@ -18,6 +21,11 @@ const NavigationBrand = css.div`
   position: relative;
   float: left;
   cursor: pointer;
+  @media (max-width: ${cssConstants.breakFive}) {
+    padding-left: 20px;
+    margin-right: 10px;
+    font-size: 14pt;
+  }
 `;
 const NavigationChild = css.span`
   font-weight: 300;
@@ -28,6 +36,18 @@ const NavigationChild = css.span`
   cursor: pointer;
   &:hover{
     text-decoration: underline;
+  }
+  @media (max-width: ${cssConstants.navigationBreakOne}) {
+    padding-left: 20px;
+    margin-right: 20px;
+  }
+  @media (max-width: ${cssConstants.breakFive}) {
+    padding-left: 10px;
+    margin-right: 10px;
+    font-size: 14pt;
+  }
+  @media (max-width: ${cssConstants.NavigationNoDisplay}) {
+    display: none;
   }
 `;
 export { NavigationContainer, NavigationBrand, NavigationChild };
--- grape-promo-app/src/global-styling/css-constants.js
+++ grape-promo-app/src/global-styling/css-constants.js
@@ -7,8 +7,12 @@ export const breakTwo = "900px";
 
 export const breakThree = "650px";
 
+export const navigationBreakOne = '550px'
+
 export const breakFour = "500px";
 
 export const breakFive = "450px";
 
 export const breakSix = "380px";
+
+export const NavigationNoDisplay = '340px'
