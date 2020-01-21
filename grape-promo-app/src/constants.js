30a08484f16c50956831c5f93282f48d27bacc92
Ash Raji <ashrafraji@hotmail.com>
2020-01-20 20:04:20 -0500

    fix grammar

--- grape-promo-app/src/constants.js
+++ grape-promo-app/src/constants.js
@@ -11,7 +11,7 @@ export const INFORM_TEXT =
 export const WHAT_SECTION_ = {
   TITLE: "What",
   CONTENT:
-    "So you’ve built something beautiful. But you want to share it with people — and you don’t just want to share it, you want people to use it. Grape will change the way people learn how to code but giving people the power to create comprehensive, cutting edge tutorials and offering a more comprehensive way to solve problems, for everyone. Grape will allow people of all skill levels to learn document and explan their coding knowledge without the overhead of subscription-based services for new learners.",
+    "So you’ve built something beautiful. But you want to share it with people — and you don’t just want to share it, you want people to use it. grape will change the way people learn how to code by giving people the power to create comprehensive, cutting edge tutorials and offering a more comprehensive way to solve problems, for everyone. grape will allow people of all skill levels to document and explan their coding knowledge without the overhead of subscription-based services for new learners.",
   BUTTON_TEXT: "Find out who is making grape…",
   BUTTON_MOBILE_TEXT: "Who's making grape…"
 };
@@ -43,15 +43,15 @@ export const NAVHOOKS = {
   HOW: "HOW"
 };
 export const EMAILPOLICY =  'mailto:srobensparadise@gmail.com'
-export const POLICY = `Grape | Coming Soon
+export const POLICY = `grape | Coming Soon
 Privacy Policy of https://grape-promo.firebaseapp.com/
 
 Privacy Policy
 Effective date: October 15, 2019
 
-Grape | Coming Soon ("us", "we", or "our") operates the https://grape-promo.firebaseapp.com/ website (the "Service").
+grape | Coming Soon ("us", "we", or "our") operates the https://grape-promo.firebaseapp.com/ website (the "Service").
 
-This page informs you of our policies regarding the collection, use, and disclosure of personal data when you use our Service and the choices you have associated with that data. Our Privacy Policy for Grape | Coming Soon is created with the help of the Free Privacy Policy Generator.
+This page informs you of our policies regarding the collection, use, and disclosure of personal data when you use our Service and the choices you have associated with that data. Our Privacy Policy for grape | Coming Soon is created with the help of the Free Privacy Policy Generator.
 
 We use your data to provide and improve the Service. By using the Service, you agree to the collection and use of information in accordance with this policy. Unless otherwise defined in this Privacy Policy, terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, accessible from https://grape-promo.firebaseapp.com/
 
@@ -79,7 +79,7 @@ Session Cookies. We use Session Cookies to operate our Service.
 Preference Cookies. We use Preference Cookies to remember your preferences and various settings.
 Security Cookies. We use Security Cookies for security purposes.
 Use of Data
-Grape | Coming Soon uses the collected data for various purposes:
+grape | Coming Soon uses the collected data for various purposes:
 
 To provide and maintain the Service
 To notify you about changes to our Service
@@ -95,14 +95,14 @@ If you are located outside Canada and choose to provide information to us, pleas
 
 Your consent to this Privacy Policy followed by your submission of such information represents your agreement to that transfer.
 
-Grape | Coming Soon will take all steps reasonably necessary to ensure that your data is treated securely and in accordance with this Privacy Policy and no transfer of your Personal Data will take place to an organization or a country unless there are adequate controls in place including the security of your data and other personal information.
+grape | Coming Soon will take all steps reasonably necessary to ensure that your data is treated securely and in accordance with this Privacy Policy and no transfer of your Personal Data will take place to an organization or a country unless there are adequate controls in place including the security of your data and other personal information.
 
 Disclosure Of Data
 Legal Requirements
-Grape | Coming Soon may disclose your Personal Data in the good faith belief that such action is necessary to:
+grape | Coming Soon may disclose your Personal Data in the good faith belief that such action is necessary to:
 
 To comply with a legal obligation
-To protect and defend the rights or property of Grape | Coming Soon
+To protect and defend the rights or property of grape | Coming Soon
 To prevent or investigate possible wrongdoing in connection with the Service
 To protect the personal safety of users of the Service or the public
 To protect against legal liability
